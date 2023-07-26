6fd227bc1696f94e8f482813a4829a0a351320a4
morishitakouki <morimori.plane.0615@gmail.com>
2023-07-26 17:14:45 +0900

    rails

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,12 +3,18 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
-
   def new
     @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク[#{task.name}]を登録しました"
+  end
+
+  def show
+    @task = Task.find(params[:id])
   end
 
   def edit
@@ -18,24 +24,18 @@ class TasksController < ApplicationController
   def update
     task = Task.find(params[:id])
     task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    redirect_to tasks_url, notice: "タスク[#{task.name}]を更新しました"
   end
 
   def destroy
     task = Task.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク#{task.name}を削除しました。"
-  end
-
-  def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    task.destroy!
+    redirect_to tasks_url, notice: "タスク[#{task.name}]を削除しました"
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:name,:description)
+    params.require(:task).permit(:name, :description)
   end
 end
